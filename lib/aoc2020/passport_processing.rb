5abe56c8f5540e08339d20b098d5a43e8ce0b21a
Robert Haines <rhaines@manchester.ac.uk>
2020-12-04 20:08:25 +0000

    Solution for day 4, part 2.

--- lib/aoc2020/passport_processing.rb
+++ lib/aoc2020/passport_processing.rb
@@ -19,6 +19,11 @@ module AOC2020
       puts "Part 1: #{valid}"
     end
 
+    def part2
+      valid = @input.count { |passport| fully_valid?(passport) }
+      puts "Part 2: #{valid}"
+    end
+
     def valid?(passport)
       return true if passport.length == 8
       return true if passport.length == 7 && !passport.key?('cid')
@@ -26,11 +31,62 @@ module AOC2020
       false
     end
 
+    def fully_valid?(passport)
+      return false unless valid?(passport)
+
+      v = validations
+      passport.map { |key, value| v[key].call(value) }.all?
+    end
+
     def parse(input)
       passports = input.split("\n\n").map(&:split)
       passports.map do |fields|
         fields.map { |f| f.split(':') }.to_h
       end
+    end
+
+    def validations
+      {
+        'byr' => lambda { |byr|
+          (1920..2002).cover?(byr.to_i)
+        },
+
+        'iyr' => lambda { |iyr|
+          (2010..2020).cover?(iyr.to_i)
+        },
+
+        'eyr' => lambda { |eyr|
+          (2020..2030).cover?(eyr.to_i)
+        },
+
+        'hgt' => lambda { |hgt|
+          height = hgt.to_i
+
+          if hgt.end_with?('cm')
+            (150..193).cover?(height)
+          elsif hgt.end_with?('in')
+            (59..76).cover?(height)
+          else
+            false
+          end
+        },
+
+        'hcl' => lambda { |hcl|
+          /^#[[:xdigit:]]{6}$/.match?(hcl)
+        },
+
+        'ecl' => lambda { |ecl|
+          %w[amb blu brn gry grn hzl oth].include?(ecl)
+        },
+
+        'pid' => lambda { |pid|
+          /^[[:digit:]]{9}$/.match?(pid)
+        },
+
+        'cid' => lambda { |_|
+          true
+        }
+      }
     end
   end
 end
--- test/passport_processing_test.rb
+++ test/passport_processing_test.rb
@@ -26,6 +26,37 @@ class AOC2020::PassportProcessingTest < MiniTest::Test
     iyr:2011 ecl:brn hgt:59in
   EOPASS
 
+  INVALID = <<~EOINVALID
+    eyr:1972 cid:100
+    hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+    iyr:2019
+    hcl:#602927 eyr:1967 hgt:170cm
+    ecl:grn pid:012533040 byr:1946
+
+    hcl:dab227 iyr:2012
+    ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+    hgt:59cm ecl:zzz
+    eyr:2038 hcl:74454a iyr:2023
+    pid:3556412378 byr:2007
+  EOINVALID
+
+  VALID = <<~EOVALID
+    pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+    hcl:#623a2f
+
+    eyr:2029 ecl:blu cid:129 byr:1989
+    iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+    hcl:#888785
+    hgt:164cm byr:2001 iyr:2015 cid:88
+    pid:545766238 ecl:hzl
+    eyr:2022
+
+    iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+  EOVALID
+
   def setup
     @pp = AOC2020::PassportProcessing.new
   end
@@ -44,5 +75,36 @@ class AOC2020::PassportProcessingTest < MiniTest::Test
     refute(@pp.valid?(passports[1]))
     assert(@pp.valid?(passports[2]))
     refute(@pp.valid?(passports[3]))
+  end
+
+  def test_validations
+    v = @pp.validations
+
+    assert(v['byr'].call('2002'))
+    refute(v['byr'].call('2003'))
+
+    assert(v['hgt'].call('60in'))
+    assert(v['hgt'].call('190cm'))
+    refute(v['hgt'].call('190in'))
+    refute(v['hgt'].call('190'))
+
+    assert(v['hcl'].call('#123abc'))
+    refute(v['hcl'].call('#123abz'))
+    refute(v['hcl'].call('123abc'))
+    refute(v['hcl'].call('#123abcd'))
+
+    assert(v['ecl'].call('brn'))
+    refute(v['ecl'].call('wat'))
+
+    assert(v['pid'].call('000000001'))
+    refute(v['pid'].call('0123456789'))
+
+    assert(v['cid'].call('000000001'))
+    assert(v['cid'].call(''))
+  end
+
+  def test_fully_valid?
+    @pp.parse(INVALID).each { |passport| refute(@pp.fully_valid?(passport)) }
+    @pp.parse(VALID).each { |passport| assert(@pp.fully_valid?(passport)) }
   end
 end
