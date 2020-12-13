e8da660f82692fc904d17b4a9404da952e0a65dd
Robert Haines <rhaines@manchester.ac.uk>
2020-12-13 18:32:17 +0000

    Solution for day 13, part 2.

--- lib/aoc2020/shuttle_search.rb
+++ lib/aoc2020/shuttle_search.rb
@@ -19,11 +19,30 @@ module AOC2020
       puts "Part 1: #{bus[0] * bus[1]}"
     end
 
+    def part2
+      puts "Part 2: #{contest}"
+    end
+
     def next_bus(input = @input)
       time = earliest_time(input)
       services = running_services(input)
       next_times = services.map { |x| [x, x - (time % x)] }
       next_times.min_by { |_, t| t }
+    end
+
+    def contest(input = @input)
+      buses = buses_with_offset(input)
+
+      # Periodicity is the bus ID; timestamp is the offset in the list of buses.
+      periodicity, timestamp = buses[0]
+      buses[1..].each do |bus|
+        while ((timestamp + bus[1]) % bus[0]) != 0
+          timestamp += periodicity # Line up the offset.
+        end
+        periodicity *= bus[0] # Multiply in the new periodicity.
+      end
+
+      timestamp
     end
 
     def earliest_time(input)
@@ -32,6 +51,12 @@ module AOC2020
 
     def running_services(input)
       input.last.split(',').map(&:to_i).delete_if(&:zero?)
+    end
+
+    def buses_with_offset(input)
+      input.last.split(',').map.with_index do |id, i|
+        id == 'x' ? nil : [id.to_i, i]
+      end.compact
     end
   end
 end
--- test/shuttle_search_test.rb
+++ test/shuttle_search_test.rb
@@ -30,4 +30,17 @@ class AOC2020::ShuttleSearchTest < MiniTest::Test
   def test_next_bus
     assert_equal([59, 5], @ss.next_bus(INPUT))
   end
+
+  def test_buses_with_offset
+    assert_equal(
+      [[7, 0], [13, 1], [59, 4], [31, 6], [19, 7]],
+      @ss.buses_with_offset(INPUT)
+    )
+  end
+
+  def test_contest
+    assert_equal(1_068_781, @ss.contest(INPUT))
+    assert_equal(3_417, @ss.contest([nil, '17,x,13,19']))
+    assert_equal(1_202_161_486, @ss.contest([nil, '1789,37,47,1889']))
+  end
 end
