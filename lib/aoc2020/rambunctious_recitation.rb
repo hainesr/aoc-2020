48a6b58d0efb6be441102c369f7cb77a140629a1
Robert Haines <rhaines@manchester.ac.uk>
2020-12-21 18:49:27 +0000

    Further optimise day 15 solutions.
    Got it down to under 15 seconds for part 2. Still think there'll be a
    non-brute-force approach...

--- lib/aoc2020/rambunctious_recitation.rb
+++ lib/aoc2020/rambunctious_recitation.rb
@@ -12,47 +12,31 @@ module AOC2020
   class RambunctiousRecitation < Day
     INPUT = [5, 1, 9, 18, 13, 8, 0].freeze
 
-    def setup
-      @game = Game.new(INPUT, 30_000_000)
-    end
-
     def part1
-      puts "Part 1: #{@game.play_to(2020)}"
+      puts "Part 1: #{play_to(2020)}"
     end
 
     def part2
-      puts "Part 2: #{@game.play_to(30_000_000)}"
+      puts "Part 2: #{play_to(30_000_000)}"
     end
 
-    class Game
-      def initialize(starting_numbers, limit = 2020)
-        @starting_numbers = starting_numbers
-        @turn = 0
-        @last_spoken = nil
-        @cache = Array.new(limit)
-      end
+    def play_to(upto, starting_numbers = INPUT)
+      turn = 0
+      last_spoken = nil
+      cache = Array.new(upto)
 
-      def play_to(upto)
-        loop do
-          turn, number = take_turn
-          return number if turn == upto
-        end
-      end
-
-      def take_turn
-        if @turn < @starting_numbers.length
-          number = @starting_numbers[@turn]
-          @cache[number] = @turn + 1
+      loop do
+        if turn < starting_numbers.length
+          last_spoken = starting_numbers[turn]
+          cache[last_spoken] = turn + 1
         else
-          history = @cache[@last_spoken]
-          @cache[@last_spoken] = @turn
-          number = (history.nil? ? 0 : @turn - history)
+          history = cache[last_spoken]
+          cache[last_spoken] = turn
+          last_spoken = (history.nil? ? 0 : turn - history)
         end
 
-        @turn += 1
-        @last_spoken = number
-
-        [@turn, number]
+        turn += 1
+        return last_spoken if turn == upto
       end
     end
   end
--- test/rambunctious_recitation_test.rb
+++ test/rambunctious_recitation_test.rb
@@ -14,21 +14,19 @@ class AOC2020::RambunctiousRecitationTest < MiniTest::Test
     @rr = AOC2020::RambunctiousRecitation.new
   end
 
-  def test_take_turn
-    game = AOC2020::RambunctiousRecitation::Game.new([0, 3, 6])
-    assert_equal([1, 0], game.take_turn)
-    assert_equal([2, 3], game.take_turn)
-    assert_equal([3, 6], game.take_turn)
-    assert_equal([4, 0], game.take_turn)
-    assert_equal([5, 3], game.take_turn)
-    assert_equal([6, 3], game.take_turn)
-    assert_equal([7, 1], game.take_turn)
-    assert_equal([8, 0], game.take_turn)
-    assert_equal([9, 4], game.take_turn)
-    assert_equal([10, 0], game.take_turn)
-  end
-
   def test_play_to
+    start = [0, 3, 6]
+    assert_equal(0, @rr.play_to(1, start))
+    assert_equal(3, @rr.play_to(2, start))
+    assert_equal(6, @rr.play_to(3, start))
+    assert_equal(0, @rr.play_to(4, start))
+    assert_equal(3, @rr.play_to(5, start))
+    assert_equal(3, @rr.play_to(6, start))
+    assert_equal(1, @rr.play_to(7, start))
+    assert_equal(0, @rr.play_to(8, start))
+    assert_equal(4, @rr.play_to(9, start))
+    assert_equal(0, @rr.play_to(10, start))
+
     [
       [[0, 3, 6], 436],
       [[1, 3, 2], 1],
@@ -37,9 +35,8 @@ class AOC2020::RambunctiousRecitationTest < MiniTest::Test
       [[2, 3, 1], 78],
       [[3, 2, 1], 438],
       [[3, 1, 2], 1836]
-    ].each do |start, number|
-      game = AOC2020::RambunctiousRecitation::Game.new(start)
-      assert_equal(number, game.play_to(2020))
+    ].each do |init, number|
+      assert_equal(number, @rr.play_to(2020, init))
     end
   end
 end
