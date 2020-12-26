ac632e039a77d8cf67813c24033acb61b422c557
Robert Haines <rhaines@manchester.ac.uk>
2020-12-26 18:22:27 +0000

    Solution for day 17, part 2.

--- lib/aoc2020/conway_cubes.rb
+++ lib/aoc2020/conway_cubes.rb
@@ -12,36 +12,61 @@ module AOC2020
   class ConwayCubes < Day
     ADJ27 = [-1, 0, 1].product([-1, 0, 1], [-1, 0, 1]).freeze
     ADJ26 = (ADJ27 - [[0, 0, 0]]).freeze
+    ADJ81 = [-1, 0, 1].product([-1, 0, 1], [-1, 0, 1], [-1, 0, 1]).freeze
+    ADJ80 = (ADJ81 - [[0, 0, 0, 0]]).freeze
 
     def setup
-      @coords = parse(read_input_file)
+      input = read_input_file
+      @coords = parse(input)
+      @coords4 = parse(input, w: true)
     end
 
     def part1
       puts "Part 1: #{boot(@coords).values.sum}"
     end
 
-    def parse(input)
+    def part2
+      puts "Part 2: #{boot(@coords4).values.sum}"
+    end
+
+    def parse(input, w: false)
       coords = {}
       input.split.each_with_index do |line, y|
         line.chars.each_with_index do |cell, x|
-          coords[[x, y, 0]] = 1 if cell == '#'
+          key = w ? [x, y, 0, 0] : [x, y, 0]
+          coords[key] = 1 if cell == '#'
         end
       end
       coords
     end
 
+    # rubocop:disable Metrics/AbcSize
+    # rubocop:disable Metrics/CyclomaticComplexity
+    # rubocop:disable Metrics/PerceivedComplexity
     def boot(coords, cycles = 6)
+      if coords.keys.first.length == 4
+        time = true
+        adj = ADJ80
+        adjs = ADJ81
+      else
+        time = false
+        adj = ADJ26
+        adjs = ADJ27
+      end
+
       cycles.times do
         n_coords = {}
 
-        coords.each do |(cx, cy, cz), _|
-          ADJ27.each do |(nx, ny, nz)|
-            coord = [x = cx + nx, y = cy + ny, z = cz + nz].freeze
+        coords.each do |(cx, cy, cz, cw), _|
+          adjs.each do |(nx, ny, nz, nw)|
+            coord = [x = cx + nx, y = cy + ny, z = cz + nz]
+            coord += [w = cw + nw] if time
             next if n_coords.key?(coord)
 
-            nbours = ADJ26.sum do |(dx, dy, dz)|
-              coords.fetch([x + dx, y + dy, z + dz], 0)
+            nbours = adj.sum do |(dx, dy, dz, dw)|
+              c = [x + dx, y + dy, z + dz]
+              c += [w + dw] if time
+              coords.fetch(c, 0)
             end
 
             n_coords[coord] = if nbours == 3
@@ -57,5 +82,8 @@ module AOC2020
 
       coords
     end
+    # rubocop:enable Metrics/PerceivedComplexity
+    # rubocop:enable Metrics/CyclomaticComplexity
+    # rubocop:enable Metrics/AbcSize
   end
 end
--- test/conway_cubes_test.rb
+++ test/conway_cubes_test.rb
@@ -33,10 +33,32 @@ class AOC2020::ConwayCubesTest < MiniTest::Test
     )
   end
 
+  def test_parse_w
+    assert_equal(
+      {
+        [1, 0, 0, 0] => 1,
+        [2, 1, 0, 0] => 1,
+        [0, 2, 0, 0] => 1,
+        [1, 2, 0, 0] => 1,
+        [2, 2, 0, 0] => 1
+      },
+      @cc.parse(INPUT, w: true)
+    )
+  end
+
   def test_boot
     coords = @cc.parse(INPUT)
     coords_save = coords.dup
     assert_equal(112, @cc.boot(coords).values.sum)
+    assert_equal(coords_save, coords)
+  end
+
+  def test_boot_w
+    coords = @cc.parse(INPUT, w: true)
+    coords_save = coords.dup
+    assert_equal(29, @cc.boot(coords, 1).values.sum)
+    assert_equal(60, @cc.boot(coords, 2).values.sum)
+    # assert_equal(848, @cc.boot(coords).values.sum)
     assert_equal(coords_save, coords)
   end
 end
