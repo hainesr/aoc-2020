e5d3709c4c22aee0c13cc689ff71c563c40eaf93
Robert Haines <rhaines@manchester.ac.uk>
2020-12-30 11:33:23 +0000

    Add code to tile and build the image for day 20.
    Needed for the solution for part 2.

--- lib/aoc2020/jurassic_jigsaw.rb
+++ lib/aoc2020/jurassic_jigsaw.rb
@@ -10,6 +10,20 @@ require 'aoc2020'
 
 module AOC2020
   class JurassicJigsaw < Day
+    OPPOSITE = {
+      top: :bottom,
+      bottom: :top,
+      left: :right,
+      right: :left
+    }.freeze
+
+    ROTATE = {
+      top: :left,
+      bottom: :right,
+      left: :top,
+      right: :bottom
+    }.freeze
+
     def setup
       @image = Image.new(read_input_file)
     end
@@ -20,7 +34,7 @@ module AOC2020
     end
 
     class Tile
-      attr_reader :borders, :id, :neighbours, :uniq_borders
+      attr_reader :borders, :id, :img, :neighbours, :uniq_borders
 
       def initialize(tile)
         id_text, *@img = tile.split("\n")
@@ -28,6 +42,79 @@ module AOC2020
         @borders = make_borders(@img)
         @uniq_borders = []
         @neighbours = {}
+      end
+
+      def make_me_top_left!
+        return false unless @uniq_borders.length == 2
+
+        new_n = @neighbours.dup
+        changed = false
+
+        # Need to flip horizontally?
+        if @uniq_borders.include?(:right)
+          new_n[:right] = new_n.delete(:left)
+          flip_h!
+          changed = true
+        end
+
+        # Need to flip vertically?
+        if @uniq_borders.include?(:bottom)
+          new_n[:bottom] = new_n.delete(:top)
+          flip_v!
+          changed = true
+        end
+
+        if changed
+          @uniq_borders = %i[top left]
+          @neighbours = new_n
+          @borders = make_borders(@img)
+        end
+
+        true
+      end
+
+      # rubocop:disable Metrics/CyclomaticComplexity
+      # rubocop:disable Metrics/PerceivedComplexity
+      def make_me_match_border!(my_border, target)
+        already_matching = @borders.select do |_, v|
+          v == target || v.reverse == target
+        end
+        current = already_matching.keys.first
+
+        img_save = @img
+
+        # Do we need to rotate?
+        if current != my_border && current != OPPOSITE[my_border]
+          rotate!
+          current = ROTATE[current]
+        end
+
+        # Do we need to flip?
+        if current != my_border
+          current == :top || current == :bottom ? flip_v! : flip_h!
+          current = OPPOSITE[current]
+        end
+
+        # Now we're on the same border but are we reversed?
+        if make_border(@img, my_border) != target
+          current == :top || current == :bottom ? flip_h! : flip_v!
+        end
+
+        @borders = make_borders(@img) if @img != img_save
+      end
+      # rubocop:enable Metrics/PerceivedComplexity
+      # rubocop:enable Metrics/CyclomaticComplexity
+
+      def flip_h!
+        @img = @img.map(&:reverse)
+      end
+
+      def flip_v!
+        @img = @img.reverse
+      end
+
+      def rotate!
+        @img = @img.map(&:chars).transpose.map(&:join)
       end
 
       private
@@ -78,13 +165,81 @@ module AOC2020
         matches.each_value do |border, tile|
           tile.uniq_borders << border unless tile.nil?
         end
+
+        @grid = nil
+        @image = nil
       end
 
       def corners
         @tiles.each_value.select { |tile| tile.uniq_borders.length == 2 }
       end
 
+      def image!
+        return @image unless @image.nil?
+
+        grid!.map do |row|
+          lines = row.map do |tile|
+            tile.img[1...-1].map { |l| l[1...-1] }
+          end
+          lines.transpose.map(&:join).join("\n")
+        end.join("\n")
+      end
+
+      def grid!(start = corners.first)
+        return @grid unless @grid.nil?
+
+        start.make_me_top_left!
+        top_row = build_line!(start, :right, :row) do |t|
+          t.uniq_borders.length == 2
+        end
+        width = top_row.length
+        # Assume a square image for now, but don't need to.
+        left_column = build_line!(start, :bottom, :column, width)
+
+        @grid = left_column[1..].map do |left|
+          lefts_left_border = if left.uniq_borders.length > 1
+                                candidates = left.uniq_borders.reject do |u|
+                                  _, t = left.neighbours[OPPOSITE[u]]
+                                  t == left_column[-2]
+                                end
+
+                                candidates.first
+                              else
+                                left.uniq_borders.first
+                              end
+          build_line!(left, OPPOSITE[lefts_left_border], :row, width)
+        end.unshift(top_row) # Put top row back.
+      end
+
       private
+
+      def build_line!(start, prev_border, line, length = nil)
+        case line
+        when :row
+          match_border_current = :left
+          match_border_prev = :right
+        when :column
+          match_border_current = :top
+          match_border_prev = :bottom
+        end
+
+        line = [start]
+        prev = start
+
+        until line.length == length
+          current_border, current = prev.neighbours[prev_border]
+          current.make_me_match_border!(
+            match_border_current, prev.borders[match_border_prev]
+          )
+          prev_border = OPPOSITE[current_border]
+          line << current
+          break if block_given? && yield(current)
+
+          prev = current
+        end
+
+        line
+      end
 
       def bitmap(str)
         str.each_char.with_index.sum do |c, i|
--- test/jurassic_jigsaw_test.rb
+++ test/jurassic_jigsaw_test.rb
@@ -10,6 +10,47 @@ require 'test_helper'
 require 'aoc2020/jurassic_jigsaw'
 
 class AOC2020::JurassicJigsawTest < MiniTest::Test
+  TILE_2311 = <<~EOTILE2311
+    Tile 2311:
+    ..##.#..#.
+    ##..#.....
+    #...##..#.
+    ####.#...#
+    ##.##.###.
+    ##...#.###
+    .#.#.#..##
+    ..#....#..
+    ###...#.#.
+    ..###..###
+  EOTILE2311
+
+  IMAGE = <<~EOIMAGE.chomp
+    .#.#..#.##...#.##..#####
+    ###....#.#....#..#......
+    ##.##.###.#.#..######...
+    ###.#####...#.#####.#..#
+    ##.#....#.##.####...#.##
+    ...########.#....#####.#
+    ....#..#...##..#.#.###..
+    .####...#..#.....#......
+    #..#.##..#..###.#.##....
+    #.####..#.####.#.#.###..
+    ###.#.#...#.######.#..##
+    #.####....##..########.#
+    ##..##.#...#...#.#.#.#..
+    ...#..#..#.#.##..###.###
+    .#.#....#.##.#...###.##.
+    ###.#...#..#.##.######..
+    .#.#.###.##.##.#..#.##..
+    .####.###.#...###.#..#.#
+    ..#.#..#..#.#.#.####.###
+    #..####...#.#.#.###.###.
+    #####..#####...###....##
+    #.##..#..#...#..####...#
+    .#.###..##..##..####.##.
+    ...###...##...#...#..###
+  EOIMAGE
+
   def setup
     @tiles = File.read('test/fixtures/jurassic_jigsaw.txt').chomp
   end
@@ -18,5 +59,73 @@ class AOC2020::JurassicJigsawTest < MiniTest::Test
     image = AOC2020::JurassicJigsaw::Image.new(@tiles)
     corners = image.corners.map(&:id).sort
     assert_equal([1171, 1951, 2971, 3079], corners)
+  end
+
+  def test_tile_flip_h!
+    tile = AOC2020::JurassicJigsaw::Tile.new(TILE_2311)
+    assert_equal(
+      [
+        '.#..#.##..',
+        '.....#..##',
+        '.#..##...#',
+        '#...#.####',
+        '.###.##.##',
+        '###.#...##',
+        '##..#.#.#.',
+        '..#....#..',
+        '.#.#...###',
+        '###..###..'
+      ],
+      tile.flip_h!
+    )
+  end
+
+  def test_tile_flip_v!
+    tile = AOC2020::JurassicJigsaw::Tile.new(TILE_2311)
+    assert_equal(
+      [
+        '..###..###',
+        '###...#.#.',
+        '..#....#..',
+        '.#.#.#..##',
+        '##...#.###',
+        '##.##.###.',
+        '####.#...#',
+        '#...##..#.',
+        '##..#.....',
+        '..##.#..#.'
+      ],
+      tile.flip_v!
+    )
+  end
+
+  def test_tile_make_me_top_left!
+    image = AOC2020::JurassicJigsaw::Image.new(@tiles)
+    tile = image.tiles[2311]
+    refute(tile.make_me_top_left!)
+
+    tile = image.tiles[3079]
+    img_save = image.tiles[3079].img.dup
+    assert(tile.make_me_top_left!)
+    assert_equal(%i[top left], tile.uniq_borders)
+    assert_equal(img_save.map(&:reverse), tile.img)
+  end
+
+  def test_grid!
+    image = AOC2020::JurassicJigsaw::Image.new(@tiles)
+    img = image.grid!
+    assert_equal(
+      [
+        [1951, 2311, 3079],
+        [2729, 1427, 2473],
+        [2971, 1489, 1171]
+      ],
+      img.map { |row| row.map(&:id) }
+    )
+  end
+
+  def test_image!
+    image = AOC2020::JurassicJigsaw::Image.new(@tiles)
+    assert_equal(IMAGE, image.image!)
   end
 end
